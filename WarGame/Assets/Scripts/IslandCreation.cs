29858ba49664db3f3595e18ae9d328db60998a1b
Codie Petersen <petersen.codie@gmail.com>
2019-04-06 23:27:14 -0700

    Extended Island Creation
    Extended the island population script to include tracking structures built on resource nodes.

--- WarGame/Assets/Scripts/IslandCreation.cs
+++ WarGame/Assets/Scripts/IslandCreation.cs
@@ -9,6 +9,7 @@ public class IslandCreation : MonoBehaviour
     //1 = lake
     //2 = mountain
     public string islandFeatures = "000000000000";
+    public string islandCollectors = "000000000000";
     public string[] tileVariations;
     public GameObject[] tilePrefabs;
     public Transform[] hexTiles;
@@ -20,16 +21,17 @@ public class IslandCreation : MonoBehaviour
         if (isRandom || islandFeatures.Length != 12)
             islandFeatures = GetRandomIsland();
 
-        PlaceTiles(islandFeatures);
+        PlaceTiles(islandFeatures, islandCollectors);
     }
 
-    public void PlaceTiles(string features)
+    public void PlaceTiles(string features, string structures)
     {
         for (int h = 0; h < hexTiles.Length; h++)
         {
             int r = Mathf.FloorToInt(Random.Range(0, 6));
             GameObject tile = null;
             string featString = islandFeatures[h].ToString();
+            string collectorString = islandCollectors[h].ToString();
 
             if (tileVariations[0].Contains(featString))
             {
@@ -45,27 +47,43 @@ public class IslandCreation : MonoBehaviour
             }
 
             tile.transform.Rotate(Vector3.up, 60 * r);
-            TurnOnResources(tile, featString);
+            TurnOnResourcesAndCollectors(tile.GetComponent<TileStats>().resourceParents, tile.GetComponent<TileStats>().collectorParents, featString, collectorString);
         }
     }
 
-    void TurnOnResources(GameObject tile, string type)
+    void TurnOnResourcesAndCollectors(GameObject[] resources, GameObject[] collectors, string type, string built)
     {
         int r = GetConvertedType(type);
+        int c = GetConvertedType(built);
 
         if (r == 1 || r == 4 || r == 5  || r == 7)
         {
-            ActivateRandomChild(tile.GetComponent<TileStats>().resourceParents[0].transform);
+            GameObject tileObject = resources[0];
+
+            if (c == 1 || c == 4 || c == 5 || c == 7)
+                tileObject = collectors[0];
+
+            ActivateRandomChild(tileObject.transform);
         }
 
         if (r == 2 || r == 4 || r == 6 || r == 7)
         {
-            ActivateRandomChild(tile.GetComponent<TileStats>().resourceParents[1].transform);
+            GameObject tileObject = resources[0];
+
+            if (c == 2 || c == 4 || c == 6 || c == 7)
+                tileObject = collectors[1];
+
+            ActivateRandomChild(resources[1].transform);
         }
 
         if (r == 3 || r == 5 || r == 6 || r == 7)
         {
-            ActivateRandomChild(tile.GetComponent<TileStats>().resourceParents[2].transform);
+            GameObject tileObject = resources[0];
+
+            if (c == 3 || c == 5 || c == 6 || c == 7)
+                tileObject = collectors[2];
+
+            ActivateRandomChild(resources[2].transform);
         }
     }
 
