7a4e828cf3f6cea9c5081caa2bd1e39caa069a76
Codie Petersen <petersen.codie@gmail.com>
2019-07-23 19:27:57 -0700

    Made Xaya Commands
    Designed and made all of the xaya json commands along with their related classes for easier serialization and deserialization. There is also a Constants class. This is for future user for nations. Needed a dictionary so I hardcoded one in the system.

---
+++ WarGame/Assets/Scripts/Namespaces/IslesOfWar/IOWCommunication.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace IslesOfWar
+{
+    namespace Communication
+    {
+        //Class variables shrunk for blockchain.
+        public class IslandBuildOrder
+        {
+            public string id;  //island identification
+            public string col; //collectors
+            public string def; //defenses
+        }
+
+        public class UnitPurchaseOrder
+        {
+            public uint[] unitCounts;
+        }
+
+        public class ResourceOrder
+        {
+            public int rsrc; //resource pool we submit too
+            public uint[] amnt; //amount we are submitting of each resource
+        }
+
+        public class BattleCommand
+        {
+            public string id;    //island identification
+            public int[][] pln;  //movement instructions
+            public uint[][] sqd; //counts of users per quad
+        }
+
+        public class DefendCommand : BattleCommand
+        {
+            public int[] flw;
+        }
+
+        public static class CommandUtility
+        {
+            public static string UpdateNation(string countryCode)
+            {
+                return string.Format("\"nat\":\"{0}\"", countryCode);
+            }
+
+            public static string CompleteIslandBuildCommands(IslandBuildOrder buildOrder)
+            {
+                JsonSerializerSettings settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore};
+                return string.Format("\"bld\":{0}", JsonConvert.SerializeObject(buildOrder, Formatting.None, settings));
+            }
+
+            public static string CompleteUnitPurchaseOrder(UnitPurchaseOrder purchaseOrder)
+            {
+                return string.Format("\"buy\":{0}",JsonConvert.SerializeObject(purchaseOrder.unitCounts));
+            }
+
+            public static string CompleteUnitPurchaseOrder(uint[] purchaseOrder)
+            {
+                return string.Format("\"buy\":{0}", JsonConvert.SerializeObject(purchaseOrder));
+            }
+
+            public static string SearchForIslands()
+            {
+                return "\"srch\":1";
+            }
+
+            public static string SubmitToResourcePot(ResourceOrder potOrder)
+            {
+                return string.Format("\"pot\":{0}", JsonConvert.SerializeObject(potOrder));
+            }
+
+            public static string SubmitToCryptoPot(string[] ids)
+            {
+                return string.Format("\"dep\":{0}",JsonConvert.SerializeObject(ids));
+            }
+
+            public static string AttackIsland(BattleCommand command)
+            {
+                return string.Format("\"attk\":{0}", JsonConvert.SerializeObject(command));
+            }
+
+            public static string DefendIsland(DefendCommand command)
+            {
+                return string.Format("\"dfnd\":{0}", JsonConvert.SerializeObject(command));
+            }
+        }
+    }
+}
--- WarGame/Assets/Scripts/UnitTesting/XayaConnectionTest.cs
+++ WarGame/Assets/Scripts/UnitTesting/XayaConnectionTest.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using IslesOfWar.Communication;
 using IslesOfWar.Callbacks;
 
 public class XayaConnectionTest : MonoBehaviour
 {
-    public string blockData;
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-            TestCallback();
+        if (Input.GetKeyDown(KeyCode.T))
+            Test();
     }
 
-    void TestCallback()
+    void Test()
     {
-        string data = "";
-        XayaProcessing.ParseStateInfo("",blockData, "", out data);
+        IslandBuildOrder buildOrder = new IslandBuildOrder();
+        buildOrder.id = "0";
+        buildOrder.col = "0123abcdABCD";
+        buildOrder.def = "0123abcdABCD";
+
+        UnitPurchaseOrder purchaseOrder = new UnitPurchaseOrder();
+        purchaseOrder.unitCounts = new uint[] { 100, 0, 0, 10, 0, 0, 10, 0, 0 };
+
+        ResourceOrder order = new ResourceOrder();
+        order.rsrc = 0;
+        order.amnt = new uint[] { 0, 1000, 1000 };
+
+        string[] depletedIslands = new string[] { "0", "A3", "BB", "1" };
+
+        BattleCommand command = new BattleCommand();
+        command.id = "0";
+        command.pln = new int[2][];
+        command.pln[0] = new int[] { 0, 1, 2, 3, 4, 5 };
+        command.pln[1] = new int[] { 0, 1, 2, 3, 4, 5 };
+        command.sqd = new uint[2][];
+        command.sqd[0] = new uint[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        command.sqd[1] = new uint[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        DefendCommand defend = new DefendCommand();
+        defend.id = "0";
+        defend.pln = command.pln;
+        defend.sqd = command.sqd;
+        defend.flw = new int[] { 0, 1 };
+
+        string data = CommandUtility.UpdateNation("ZW");
+        Debug.Log(data);
+
+        data = CommandUtility.CompleteIslandBuildCommands(buildOrder);
+        Debug.Log(data);
+
+        data = CommandUtility.CompleteUnitPurchaseOrder(purchaseOrder);
+        Debug.Log(data);
+
+        data = CommandUtility.SearchForIslands();
+        Debug.Log(data);
+
+        data = CommandUtility.SubmitToResourcePot(order);
+        Debug.Log(data);
+
+        data = CommandUtility.SubmitToCryptoPot(depletedIslands);
+        Debug.Log(data);
+
+        data = CommandUtility.AttackIsland(command);
+        Debug.Log(data);
+
+        data = CommandUtility.DefendIsland(defend);
         Debug.Log(data);
     }
 }
