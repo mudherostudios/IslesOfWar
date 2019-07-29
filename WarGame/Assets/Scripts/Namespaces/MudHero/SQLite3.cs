0e9587c4968c70d7714426488313c702ff575c67
Codie Petersen <petersen.codie@gmail.com>
2019-07-29 02:04:57 -0700

    DBManager Created
    Created the database manager and supporting utility functions. Didn't get as much as I wanted to today. Started at six so very late. But the database management will be easy now. I also have made a bunch of test cases for each of the functions just to make sure.  Next push will be a bunch of storing data. I'll probably test it on the mover gamespace name since it has more moves than vuteka does. So it will be a pretty robust test I think. Once I know it works or have figured out what needs to be added/changed, I will move <--eh see what i did there... into making the db updaters for each table. I think... that should be easy. I might run into problems though, but I'm hoping the moves storage is done pretty quickly, as it's just raw block data I shouldn't have too many problems. I should probably integrate starting off at the last downloaded/processed block somepoint tomorrow as well.

---
+++ WarGame/Assets/Scripts/Namespaces/MudHero/SQLite3.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+namespace MudHero
+{
+    namespace SQLite3
+    {
+        public static class DBCommandUtility
+        {
+            public static string GetInsertCommand(string table, string[] ids, object[] values)
+            {
+                string tableIDs = string.Format("({0})",string.Join(",", ids));
+                string tablesValues = string.Format("({0})", GetSQLiteVarField(values));
+                return string.Format("INSERT OR IGNORE INTO {0} {1} VALUES {2}", table, tableIDs, tablesValues);
+            }
+
+            public static string GetUpdateCommand(string table, string searchKey, object searchValue, string[] keysToUpdate, object[] values)
+            {
+                string whereStatement = "";
+
+                if (searchKey != null)
+                {
+                    if (searchKey.Length > 0)
+                    {
+                        if (searchValue.GetType() == typeof(string))
+                            whereStatement = string.Format("WHERE {0} = \"{1}\"", searchKey, searchValue);
+                        else if (searchValue.GetType() == typeof(int) || searchValue.GetType() == typeof(long))
+                            whereStatement = string.Format("WHERE {0} = {1}", searchKey, searchValue);
+                    }
+                }
+
+                return string.Format("UPDATE {0} SET {1} {2}", table, GetSQLiteEqualityField(keysToUpdate, values), whereStatement);
+            }
+
+            public static string GetSelectCommand(string table, string[] idsToGet)
+            {
+                string selectionIDs = string.Join(",", idsToGet);
+                return string.Format("SELECT {0} FROM {1}", selectionIDs, table);
+            }
+
+            public static string GetConditionalSelectCommand(string table, string[] conditionalIDs, object[] conditionalValues, string[] idsToGet)
+            {
+                string whereStatement = "";
+
+                if (conditionalIDs != null)
+                    if (conditionalIDs.Length > 0)
+                        whereStatement = string.Format("WHERE {0}", GetSQLiteEqualityField(conditionalIDs, conditionalValues));
+
+                string selectionIDs = string.Join(",", idsToGet);
+                return string.Format("SELECT {0} FROM {1} {2}", selectionIDs, table, whereStatement);
+            }
+
+            static string GetSQLiteVarField(object[] values)
+            {
+                string field = "";
+
+                for (int v = 0; v < values.Length; v++)
+                {
+                    if (values[v] != null)
+                    {
+                        var type = values[v].GetType();
+
+                        if (type == typeof(string))
+                            field += string.Format("\"{0}\",", values[v]);
+                        else if (type == typeof(long) || type == typeof(double))
+                            field += values[v] + ",";
+                    }
+                    else
+                    {
+                        field += "NULL,";
+                    }
+                }
+
+                field = field.Remove(field.Length - 1, 1);
+                return field;
+            }
+
+            static string GetSQLiteEqualityField(string[] ids, object[] values)
+            {
+                string field = "";
+
+                for(int v = 0; v < values.Length; v++)
+                {
+                    if (values[v] != null)
+                    {
+                        var type = values[v].GetType();
+
+                        if (type == typeof(string))
+                            field += string.Format("{0}=\"{1}\",", ids[v], values[v]);
+                        else if (type == typeof(long) || type == typeof(double))
+                            field += string.Format("{0}={1},", ids[v], values[v]);
+                    }
+                    else
+                    {
+                        field += string.Format("{0}=NULL,", ids[v]);
+                    }
+                }
+
+                field = field.Remove(field.Length - 1, 1);
+
+                return field;
+            }
+        }
+
+        public class DBManager
+        {
+            string dbPath;
+            string connection;
+            IDbConnection dbConnection;
+            IDataReader dbReader;
+            IDbCommand createCommand;
+            IDbCommand insertCommand;
+            IDbCommand updateCommand;
+            IDbCommand readerCommand;
+
+            public DBManager(string databasePath, string dbName)
+            {
+                dbPath = databasePath;
+                connection = string.Format("URI=file:{0}/{1}", dbPath, dbName);
+
+                if (!Directory.Exists(dbPath))
+                    Directory.CreateDirectory(databasePath);
+
+                dbConnection = new SqliteConnection(connection);
+            }
+
+            public void Open()
+            {
+                dbConnection.Open();
+            }
+
+            public void Close()
+            {
+                dbConnection.Close();
+            }
+
+            public void CreateTable(string table, string tableDefinitions)
+            {
+                if (dbConnection.State == ConnectionState.Open)
+                {
+                    if (createCommand == null)
+                        createCommand = dbConnection.CreateCommand();
+                    try
+                    {
+                        createCommand.CommandText = string.Format("CREATE TABLE IF NOT EXISTS {0}({1})", table, tableDefinitions);
+                        createCommand.ExecuteReader();
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.Log(string.Format("Failed to Create Table. \n{0}", e.Message));
+                    }
+                }
+            }
+
+            public void Insert(string tableName, string[] tableIDs, object[] values)
+            {
+                if (dbConnection.State == ConnectionState.Open)
+                {
+                    if (insertCommand == null)
+                        insertCommand = dbConnection.CreateCommand();
+                    try
+                    {
+                        insertCommand.CommandText = DBCommandUtility.GetInsertCommand(tableName, tableIDs, values);
+                        insertCommand.ExecuteNonQuery();
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.Log(string.Format("Failed to Insert Row.\n{0}", e.Message));
+                    }
+                }
+            }
+
+            public void Update(string tableName, string searchKey, object searchValue, string[] keysToUpdate, object[] values)
+            {
+                if (dbConnection.State == ConnectionState.Open)
+                {
+                    if (updateCommand == null)
+                        updateCommand = dbConnection.CreateCommand();
+
+                    try
+                    {
+                        updateCommand.CommandText = DBCommandUtility.GetUpdateCommand(tableName, searchKey, searchValue, keysToUpdate, values);
+                        updateCommand.ExecuteReader();
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.Log(string.Format("Failed to Update Row.\n{0}",e.Message));
+                    }
+                }
+            }
+            
+            public object[][] GetFromTable(string table, string[] conditionalIDs = null, object[] conditionalValues = null, string[] idsToGet = null)
+            {
+                List<object[]> rowObjects = new List<object[]>();
+
+                if (dbConnection.State == ConnectionState.Open)
+                {
+                    if (readerCommand == null)
+                        readerCommand = dbConnection.CreateCommand();
+
+                    try
+                    {
+                        //Ifs are done this way because last string[] would mean all parameters are being used meaning we are doing conditional
+                        if(idsToGet != null)
+                            readerCommand.CommandText = DBCommandUtility.GetConditionalSelectCommand(table, conditionalIDs, conditionalValues, idsToGet);
+                        else if(conditionalIDs != null) //ConditionalIDs become just regular ids to get.
+                            readerCommand.CommandText = DBCommandUtility.GetSelectCommand(table, conditionalIDs);
+                        else
+                            readerCommand.CommandText = DBCommandUtility.GetSelectCommand(table, new string[] { "*" });
+
+                        dbReader = readerCommand.ExecuteReader();
+
+                        while (dbReader.Read())
+                        {
+                            object[] row = new object[dbReader.FieldCount];
+
+                            for (int w = 0; w < row.Length; w++)
+                            {
+                                row[w] = dbReader.GetValue(w);
+                            }
+
+                            rowObjects.Add(row);
+                        }
+
+                        dbReader.Close();
+                        return rowObjects.ToArray();
+                    }
+                    catch (Exception e)
+                    {
+                        dbReader.Close();
+                        Debug.Log(string.Format("Failed to Get Objects.\n{0}", e.Message));
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ WarGame/Assets/Scripts/UnitTesting/Tests.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TestNameUpdates & TestActionParsing
+using IslesOfWar.Communication;
+using IslesOfWar.GameStateProcessing;
+
+//TestActionParsing
+using MudHero.XayaProcessing;
+using Newtonsoft.Json;
+
+//TestSQLiteDB
+using Mono.Data.Sqlite;
+using System.Data;
+using System.IO;
+
+//TestSQLCommandStrings & TestDBManager
+using MudHero.SQLite3;
+
+public class Tests : MonoBehaviour
+{
+    public string blockData; //should have valid block data but not valid move data
+    public string validBlockData; //should have valid block data and move data
+    public string databasePath = "/database/";
+    public string dbName = "";
+    public bool DEBUG = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            TestNameUpdates();
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            TestActionParsing();
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            TestSQLiteDB();
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            TestSQLCommandStrings();
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            TestDBManager();
+    }
+
+    void TestNameUpdates()
+    {
+        IslandBuildOrder buildOrder = new IslandBuildOrder();
+        buildOrder.id = "0";
+        buildOrder.col = "0123abcdABCD";
+        buildOrder.def = "0123abcdABCD";
+
+        UnitPurchaseOrder purchaseOrder = new UnitPurchaseOrder();
+        purchaseOrder.unitCounts = new uint[] { 100, 0, 0, 10, 0, 0, 10, 0, 0 };
+
+        ResourceOrder order = new ResourceOrder();
+        order.rsrc = 0;
+        order.amnt = new uint[] { 0, 1000, 1000 };
+
+        string[] depletedIslands = new string[] { "0", "A3", "BB", "1" };
+
+        BattleCommand command = new BattleCommand();
+        command.id = "0";
+        command.pln = new int[2][];
+        command.pln[0] = new int[] { 0, 1, 2, 3, 4, 5 };
+        command.pln[1] = new int[] { 0, 1, 2, 3, 4, 5 };
+        command.sqd = new uint[2][];
+        command.sqd[0] = new uint[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        command.sqd[1] = new uint[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        BattleCommand defend = new BattleCommand();
+        defend.id = "0";
+        defend.pln = command.pln;
+        defend.sqd = command.sqd;
+        defend.flw = new int[] { 0, 1 };
+
+        string nation = CommandUtility.UpdateNation("ZW");
+        string build = CommandUtility.CompleteIslandBuildCommands(buildOrder);
+        string purchase = CommandUtility.CompleteUnitPurchaseOrder(purchaseOrder);
+        string search = CommandUtility.SearchForIslands();
+        string pot = CommandUtility.SubmitToResourcePot(order);
+        string crypto = CommandUtility.SubmitToCryptoPot(depletedIslands);
+        string atk = CommandUtility.AttackIsland(command);
+        string def = CommandUtility.DefendIsland(defend);
+
+        Debug.Log(string.Format("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n", nation, build, purchase, search, pot, crypto, atk, def));
+    }
+
+    void TestActionParsing()
+    {
+        Actions actions = ActionParser.JsonToActions(blockData);
+        Actions validActions = ActionParser.JsonToActions(validBlockData);
+        PlayerActions playerActions = ActionParser.ParseMove(JsonConvert.SerializeObject(validActions.moves[0].move));
+
+        Debug.Log(actions.moves.Count);
+        Debug.Log(actions.rngseed);
+
+        for (int i = 0; i < actions.moves[0].inputs.Count; i++)
+        {
+            Debug.Log(actions.moves[0].inputs[i].txid);
+            Debug.Log(actions.moves[0].inputs[i].vout);
+        }
+
+        Debug.Log(JsonConvert.SerializeObject(actions.moves[0].move));
+        Debug.Log(actions.moves[0].name);
+        Debug.Log(actions.moves[0].txid);
+        Debug.Log(actions.admin);
+
+        if (playerActions.nat != null)
+        {
+            string islandIDs = "";
+            foreach (string id in playerActions.dep)
+            {
+                islandIDs += "ID: " + id + "\n";
+            }
+
+            Debug.Log
+            (
+                "Nation: " + playerActions.nat + "\n\n" +
+                "-Build Order-" + "\n" +
+                "ID: " + playerActions.bld.id + "\n" +
+                "Collectors: " + playerActions.bld.col + "\n" +
+                "Defenses: " + playerActions.bld.def + "\n" + "------\n\n" +
+                "Unit Purchase: " + playerActions.buy[0] + "\n" +
+                "Search Islands: " + playerActions.srch + "\n\n" +
+                "-Resource Submissions-" + "\n" +
+                "Resource Type: " + playerActions.pot.rsrc + "\n" +
+                "Amounts: " + playerActions.pot.amnt[1] + "\n" + "------\n\n" +
+                "-Depleted Islands-" + "\n" +
+                islandIDs + "--------\n\n" +
+                "Attack Plan Island: " + playerActions.attk.id + "\n" +
+                "Defend Order Island: " + playerActions.dfnd.id + "\n\n"
+            );
+        }
+        else
+        {
+            Debug.Log("Failed to Parse Player Actions");
+        }
+    }
+
+    void TestSQLiteDB()
+    {
+        IDbCommand dbCommand;
+        IDbCommand otherCommand;
+        IDbCommand readCommand;
+        IDataReader reader;
+        
+        string folderPath = string.Format("{0}{1}", Application.persistentDataPath, databasePath);
+        string connection = string.Format("URI=file:{0}{1}",folderPath,dbName);
+
+        if (!Directory.Exists(folderPath))
+        {
+            if(DEBUG)
+                Debug.Log(string.Format("Creating Folder at {0}.", folderPath));
+            Directory.CreateDirectory(folderPath);
+        }
+        else if(Directory.Exists(folderPath) && DEBUG)
+        {
+            
+            Debug.Log("Folder Already Exists. Not Recreating.");
+        }
+
+        if(DEBUG)
+            Debug.Log(string.Format("Attempting database connection with {0}.", connection));
+
+        IDbConnection dbConnection = new SqliteConnection(connection);
+        dbConnection.Open();
+
+        if (DEBUG)
+            Debug.Log("Creating Table.");
+
+        dbCommand = dbConnection.CreateCommand();
+        string sqlCreateTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+        dbCommand.CommandText = sqlCreateTable;
+        dbCommand.ExecuteReader();
+
+        if (DEBUG)
+            Debug.Log("Updating Table.");
+
+        otherCommand = dbConnection.CreateCommand();
+        otherCommand.CommandText = "INSERT OR REPLACE INTO my_table (id, val) VALUES (0, 5)";
+        otherCommand.ExecuteNonQuery();
+
+        if (DEBUG)
+            Debug.Log("Reading Database");
+
+        readCommand = dbConnection.CreateCommand();
+        string dbQuery = "SELECT * FROM my_table";
+        readCommand.CommandText = dbQuery;
+        reader = readCommand.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Debug.Log(string.Format("id: {0}\nval: {1}\n", reader[0].ToString(), reader[1].ToString()));
+        }
+
+        reader.Close();
+        dbConnection.Close();
+    }
+
+    void TestSQLCommandStrings()
+    {
+        string table = "islands";
+        string tableDefinitions = "id TEXT PRIMARY KEY, version INTEGER, lastblock INTEGER, owner TEXT, features TEXT, collectors TEXT, defenses TEXT";
+        string[] tableIDs = new string[] { "id", "version", "lastblock", "owner", "features", "collectors", "defenses"};
+        object[] tableValues = new object[] { "asdf", (long)1, (long)100, "asdf", null, "0123abcdABCD", "123456701234"};
+
+        Debug.Log(string.Format("CREATE TABLE IF NOT EXISTS {0}({1})", table, tableDefinitions));
+        Debug.Log(DBCommandUtility.GetInsertCommand(table, tableIDs, tableValues));
+        Debug.Log(DBCommandUtility.GetUpdateCommand(table, "id", "asdf", new string[] {"defenses", "owner", "lastblock"}, new object[] {"1337",null, (long)200}));
+        Debug.Log(DBCommandUtility.GetConditionalSelectCommand(table, new string[] { "id", "defenses" }, new object[] { "asdf", "123456" }, tableIDs));
+        Debug.Log(DBCommandUtility.GetSelectCommand(table, tableIDs));
+    }
+
+    void TestDBManager()
+    {
+        string table = "islands";
+        string tableDefinitions = "id TEXT PRIMARY KEY, version INTEGER, lastblock INTEGER, owner TEXT, features TEXT, collectors TEXT, defenses TEXT";
+        string[] tableIDs = new string[] { "id", "version", "lastblock", "owner", "features", "collectors", "defenses" };
+        object[] tableValues = new object[] { "asdf", (long)1, (long)100, "asdf", null, "0123abcdABCD", "123456701234" };
+
+        DBManager database = new DBManager(Application.persistentDataPath, dbName);
+        database.Open();
+        database.CreateTable(table, tableDefinitions);
+        database.Insert(table, tableIDs, tableValues);
+        database.Update(table, "id", "asdf", new string[] { "features" }, new object[] { "I am the features." });
+        object[][] objects = database.GetFromTable(table, new string[] { "id" }, new string[] { "asdf" }, tableIDs);
+        objects = database.GetFromTable(table, tableIDs);
+        objects = database.GetFromTable(table);
+        database.Close();
+    }
+}
