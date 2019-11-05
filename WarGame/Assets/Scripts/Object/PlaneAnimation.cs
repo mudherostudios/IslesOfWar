4e98d4f13744716fe84a8b7b3a627d8e2c180dc3
Codie Petersen <petersen.codie@gmail.com>
2019-11-05 12:09:47 -0800

    Fixed all animations and sizes.
    Animations for tanks and planes have been changed to scripted animations and sizes have been corrected to fit inside the tiles.

---
+++ WarGame/Assets/Scripts/Object/PlaneAnimation.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneAnimation : MonoBehaviour
+{
+    public Vector3 planeMovementMin, planeMovementMax;
+    public Vector3 propellerRotation;
+    public float moveTime, resetDistance;
+    public Transform[] propellers;
+    private Vector3 moveToPos, startPos, parentPos;
+    private float moveStart;
+
+
+    public void Start()
+    {
+        startPos = transform.localPosition;
+        parentPos = transform.parent.position;
+        moveToPos = GetMovePosition();
+        moveStart = Time.time;
+    }
+
+    public void Update()
+    {
+        RotatePropellers();
+
+        if (Vector3.Distance(parentPos, transform.parent.position) > 0.001)
+        {
+            parentPos = transform.parent.position;
+            moveToPos = GetMovePosition();
+            moveStart = Time.time;
+        }
+
+        if (Vector3.Distance(transform.position, moveToPos) < resetDistance)
+        {
+            moveToPos = GetMovePosition();
+            moveStart = Time.time;
+        }
+        else
+        {
+            MoveToPosition();
+        }
+    }
+
+    void MoveToPosition()
+    {
+        float currentTime = Time.time - moveStart;
+        transform.position = Vector3.Lerp(transform.position, moveToPos, currentTime / moveTime);
+    }
+
+    Vector3 GetMovePosition()
+    {
+        Vector3 nextMove = Vector3.zero;
+        Vector3 origin = transform.parent.TransformPoint(startPos);
+        nextMove.x = Random.Range(planeMovementMin.x, planeMovementMax.x) + origin.x;
+        nextMove.y = Random.Range(planeMovementMin.y, planeMovementMax.y) + origin.y;
+        nextMove.z = Random.Range(planeMovementMin.z, planeMovementMax.z) + origin.z;
+        
+        return nextMove;
+    }
+
+    void RotatePropellers()
+    {
+        for (int p = 0; p < propellers.Length; p++)
+        {
+            propellers[p].Rotate(propellerRotation);
+        }
+    }
+}
---
+++ WarGame/Assets/Scripts/Object/TankAnimation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAnimation : MonoBehaviour
+{
+    public Transform turret;
+    public Vector3 axis;
+    public float minRotSpeed, maxRotSpeed, minRotTime, maxRotTime, minWaitTime, maxWaitTime;
+
+    private float rotSpeed, rotTime, waitTime;
+    private bool rotPositive;
+    private float rotStartTime, waitStartTime, currentWaitTimer;
+    private bool isWaiting;
+
+    private void Start()
+    {
+        rotTime = Random.Range(minRotTime, maxRotTime);
+        rotSpeed = Random.Range(minRotSpeed, maxRotSpeed);
+        waitTime = Random.Range(minWaitTime, maxWaitTime);
+        rotPositive = true;
+        isWaiting = false;
+    }
+
+    private void Update()
+    {
+        if (!isWaiting)
+        {
+            float directionModifier = -1.0f;
+            if (rotPositive)
+                directionModifier = 1.0f;
+
+            turret.Rotate(axis, directionModifier * rotSpeed);
+
+            if (Time.time - rotStartTime > rotTime)
+            {
+                waitTime = Random.Range(minWaitTime, maxWaitTime);
+                isWaiting = true;
+                waitStartTime = Time.time;
+            }
+        }
+
+        if(isWaiting)
+        {
+            currentWaitTimer = Time.time - waitStartTime;
+
+            if (currentWaitTimer > waitTime)
+            {
+                isWaiting = false;
+                currentWaitTimer = 0.0f;
+
+                rotTime = Random.Range(minRotTime, maxRotTime);
+                rotSpeed = Random.Range(minRotSpeed, maxRotSpeed);
+                rotStartTime = Time.time;
+
+                int dir = Mathf.RoundToInt(Random.Range(0, 2));
+                rotPositive = true;
+                if (dir == 0)
+                    rotPositive = false;
+            }
+        }
+    }
+}
