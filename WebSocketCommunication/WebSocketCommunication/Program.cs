c102feef4fe3d68a5ba5a32cf40fc6a981ba9e39
Codie-Petersen <>
2020-04-04 02:43:47 -0700

    Adding WebSocketCommunication Project
    Adding a WebSocketCommuncation project. It will be used for the player trading. I was having problems integrating some of the websocket library stuff directly into unity, turns out its because of some features that Unity doesn't implement.
    
    Regardless, probably better if its a separate library anyways as a plugin, more useful.

---
+++ WebSocketCommunication/WebSocketCommunication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketCommunication;
+
+class Program
+{
+    static string url = "ws://echo.websocket.org";//"wss://livetrade.islesofwar.online";
+
+    static void Main(string[] args)
+    {
+        using (WebSocket socket = new WebSocket(url))
+        {
+            socket.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+
+            socket.OnMessage += Utility.HandleSocketMessage;
+            socket.OnError += Utility.HandleSocketError;
+            socket.OnClose += Utility.HandleSocketClose;
+
+            socket.Connect();
+            socket.Send("{\"msg\":\"Hello!\"}");
+            Console.ReadLine();
+            socket.Close(CloseStatusCode.Normal, "{\"msg\":\"Client has closed connection.\"}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebSocketCommunication/WebSocketCommunication/WebSocketCommunication.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft;
+using WebSocketSharp;
+
+namespace WebSocketCommunication
+{
+    public static class Utility
+    {
+        public static void HandleSocketMessage(object sender, MessageEventArgs message)
+        {
+            SocketMessage dataObject;
+            if (TryJsonParse(message.Data, out dataObject))
+            {
+                //Do something something here with data.
+                //Needs to give the data to unity if its transaction data.
+                //Trying to keep this generic though.
+                Console.WriteLine(message.Data);
+            }
+            else
+            {
+                string badDataMessage = string.Format("Bad Data: {0}", message.Data);
+                Console.WriteLine(badDataMessage);
+            }
+        }
+
+        public static void HandleSocketError(object sender, ErrorEventArgs error)
+        {
+            Console.WriteLine(error.Message);
+        }
+
+        public static void HandleSocketClose(object sender, CloseEventArgs close)
+        {
+            Console.WriteLine(close.Reason);
+        }
+
+        public static bool TryJsonParse<T>(string data, out T obj)
+        {
+            try
+            {
+                obj = JsonConvert.DeserializeObject<T>(data);
+                return true;
+            }
+            catch (JsonException jsonException)
+            {
+                Console.WriteLine(jsonException.Message);
+                obj = default(T);
+                return false;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                obj = default(T);
+                return false;
+            }
+        }
+    }
+
+    public class SocketMessage
+    {
+        public string userID;       //WebSocket UserID for communication.
+        public object payload;      //A generic object slot.
+        public PayloadType type;    //A type to note how to handle the payload.
+
+        public SocketMessage(string _userID, object _payload, PayloadType _type)
+        {
+            userID = _userID;
+            payload = _payload;
+            type = _type;
+        }
+    }
+
+    public class TransactionData
+    {
+        public TransactionPhase phase;
+        public object contractProposal;
+        public string contractHex;
+    }
+
+    public enum TransactionPhase
+    {
+        NONE,                   
+        PROPOSAL,               //Send a contract proposal to seller
+        SELLER_REJECT,          //Send rejection message bidder reforms proposal
+        SELLER_SIGN,            //Seller sends signed contract
+        BIDDER_REJECT_SIGN,     //Bidder rejects signatures sends contract back
+        BIDDER_CONFIRMED_SIGN,  //Bidder has signed and sent contract to blockchain
+        REJECT_PERMANENT        //A party ends negotiations
+    }
+    
+    public enum PayloadType
+    {
+        NONE,
+        TRANSACTION,
+        CHAT
+    }
+}
